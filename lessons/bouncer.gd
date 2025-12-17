a22598d112599b1a5eca07e4cc99656944479d2b
Beanofbean <jubarrioscastro@s.sfusd.edu>
2025-12-16 23:03:41 -0800

    i think 13 14 and 15

--- lessons/bouncer.gd
+++ lessons/bouncer.gd
@@ -2,12 +2,12 @@ extends CharacterBody2D
 
 @export var max_speed := 600.0
 @export var acceleration := 1200.0
+@export var avoidance_strength := 30000.0
 
-@onready var bouncer: CharacterBody2D = %Bouncer
-@onready var runner_visual: RunnerVisual = %RunnerVisualPurple
 @onready var dust: GPUParticles2D = %dust
-
+@onready var runner_visual: RunnerVisual = %RunnerVisualPurple
 @onready var hit_box: Area2D = %HitBox
+@onready var raycast: Node2D = %Raycast
 
 
 
@@ -16,6 +16,8 @@ func _ready() -> void:
 		if body is Runner:
 			get_tree().reload_current_scene.call_deferred()
 	)
+	for raycast: RayCast2D in raycast.get_children():
+		raycast.add_exception(self)
 
 
 func _physics_process(delta: float) -> void:
@@ -24,12 +26,14 @@ func _physics_process(delta: float) -> void:
 	var speed := max_speed if distance > 100 else max_speed * distance / 100
 
 	var desired_velocity := direction * speed
+	desired_velocity += calculate_avoidance_force() * delta
+
 	velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 	move_and_slide()
 
 	if velocity.length() > 10.0:
-		var angle := rotate_toward(runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
-		runner_visual.angle = angle
+		runner_visual.angle = rotate_toward(runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
+		raycast.rotation = runner_visual.angle
 
 		var current_speed_percent := velocity.length() / max_speed
 		runner_visual.animation_name = (
@@ -46,3 +50,20 @@ func _physics_process(delta: float) -> void:
 
 func get_global_player_position() -> Vector2:
 	return get_tree().root.get_node("Game/Runner").global_position
+
+
+func calculate_avoidance_force() -> Vector2:
+	var avoidance_force := Vector2.ZERO
+
+	for raycast: RayCast2D in raycast.get_children():
+		if raycast.is_colliding():
+			var collision_position := raycast.get_collision_point()
+			var direction_away_from_obstacle := collision_position.direction_to(raycast.global_position)
+
+			var ray_length := raycast.target_position.length()
+			var intensity := 1.0 - collision_position.distance_to(raycast.global_position) / ray_length
+
+			var force := direction_away_from_obstacle * avoidance_strength * intensity
+			avoidance_force += force
+
+	return avoidance_force
